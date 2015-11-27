62e5d7873ab63367daab94b9d708b4afa8b46452
Oscar Delgadillo Tovar <oscar_oz_d@hotmail.com>
2015-11-26 20:31:59 -0400

    Classes created for the game

---
+++ js/bsGame.js
@@ -0,0 +1,18 @@
+/**
+ * Created by oscardelgadillo on 11/20/2015.
+ */
+
+var BSGame = function(){
+    this.name = 'Battle Ship Game';
+    this.players = [];
+    this.players.push(new Player('Pablo'));
+    this.players.push(new Player('Pedro'));
+
+    this.start = function (){
+
+    };
+    //var player1 = new Player();
+    //var player2 = new Player();
+
+};
+
