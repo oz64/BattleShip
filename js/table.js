2d755bb13e241f8975ba38976cb874c06a120ec6
Maria <maria.alcocer@fundacion-jala.org>
2015-12-11 17:12:45 -0400

    Add shots on table and ships

--- js/table.js
+++ js/table.js
@@ -128,5 +128,47 @@ Table.prototype.fillShips = function (coorX,coorY,indexS,orientation){
             coorY++;
     }
 };
+Table.prototype.shot2ship = function(positionX, positionY)
+{
+    if (this.cell[positionX][positionY] != '0')
+    {
+        var s = this.cell[positionX][positionY];
+        this.Ships[s-1].shotShip(positionX,positionY);
+        this.cell[positionX][positionY] = 'X';
+    }
+    else
+    {
+        alert('Shot missed.');
+        this.cell[positionX][positionY] = 'X';
+    }
+
+};
+
+Table.prototype.drawTable = function () {
+    var w = this.sizeTable * 50;
+    //var styleStr  =  '<style>' +
+    //    '.cell {background: green; width: 50px; height: 50px; display: inline-block;}' +
+    //    '.container {background-color: red; width: '+w +'px;}' +
+    //    '</style>';
+
+    var containerStr = '<div class = "table" style="width: '+w+'px;"></div>';
+    var cellStr = '<div class = "cell"></div>';
+
+    var containerEl = $(containerStr); //html element
+    //var styleEl = $(styleStr);
+
+    for(var i = 0, total = this.sizeTable * this.sizeTable; i < total; i++){
+        var cellEl = $(cellStr);
+        cellEl.on('click',function(e){
+            console.log(e);
+            console.log('you clicked on a cell');
+            $(e.target).css('background','yellow');
+
+        });
+        containerEl.append(cellEl);
+    }
+    //$('body').html(styleEl);
+    $('body').append(containerEl);
+};
 
 
